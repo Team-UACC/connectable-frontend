98ca586d67802d299f89b766297b664916740b36
iHoHyeon <hyunee169@naver.com>
2022-07-06 10:45:16 +0900

    💄 [main/Design] Header UI hover 스타일 작성

--- apps/main/components/Header.tsx
+++ apps/main/components/Header.tsx
@@ -1,18 +1,52 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import { MouseEvent } from 'react';
 
 export default function Header() {
   return (
     <header className="relative flex justify-between w-full bg-white backdrop-blur-md">
-      <nav className="relative flex justify-between w-full py-[2.125rem] ">
-        <div className="text-xl ">LOGO</div>
-        <div className="flex justify-between text-base leading-6 w-52">
-          <Image src="/images/ticket.svg" alt="ticket" width={24} height={24} />
-          <Image src="/images/market.svg" alt="ticket" width={24} height={24} />
-          <Image src="/images/notification.svg" alt="ticket" width={24} height={24} />
-          <Image src="/images/login.svg" alt="ticket" width={24} height={24} />
-          <Image src="/images/menu.svg" alt="ticket" width={24} height={24} />
+      <nav className="relative flex justify-between w-full py-[1.5rem] ">
+        <div className="flex flex-col justify-center">
+          <Link href="/">
+            <div className="text-2xl cursor-pointer">LOGO</div>
+          </Link>
+        </div>
+        <div className="flex justify-between  w-[15rem]">
+          <NavIcon src="/images/ticket.svg" alt="ticket" href="/events" />
+          <NavIcon src="/images/market.svg" alt="market" href="/events" />
+          <NavIcon src="/images/notification.svg" alt="notification" href="/events" />
+          <NavIcon src="/images/login.svg" alt="login" href="/events" />
+          <NavIcon src="/images/menu.svg" alt="menu" href="/events" />
         </div>
       </nav>
     </header>
   );
 }
+
+type NavIconButtonProps = {
+  src: string;
+  alt: string;
+  href?: string;
+  width?: number;
+  height?: number;
+  onClick?: (e: MouseEvent) => void;
+};
+
+const NavIcon = ({ src, alt, href, width = 24, height = 24, onClick }: NavIconButtonProps) => {
+  if (!href)
+    return (
+      <button onClick={onClick}>
+        <div className="flex flex-col justify-center cursor-pointer h-[3rem] w-[3rem] rounded-full transition-all ease-in-out hover:scale-110 hover:bg-gray-100">
+          <Image src={src} alt={alt} width={width} height={height} />
+        </div>
+      </button>
+    );
+  else
+    return (
+      <Link href={href}>
+        <div className="flex flex-col justify-center cursor-pointer h-[3rem] w-[3rem] rounded-full transition-all ease-in-out hover:scale-110 hover:bg-gray-100">
+          <Image src={src} alt={alt} width={width} height={height} />
+        </div>
+      </Link>
+    );
+};
