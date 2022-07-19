24a4a03bbc41115574783d807abe187c3a6bb6e1
iHoHyeon <hyunee169@naver.com>
2022-07-19 12:19:29 +0900

    ✨ [main/Feat] UACC-118 UACC-120 유저 데이터 유효성 검증

---
+++ apps/main/src/pages/api/users/validation.ts
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'GET') {
+    res.json({
+      available: true,
+    });
+  }
+};
