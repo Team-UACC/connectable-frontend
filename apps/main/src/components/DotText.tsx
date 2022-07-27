5cabf4204aebd3c5c61205bb8964c6bcc3049f8e
iHoHyeon <hyunee169@naver.com>
2022-07-27 16:45:37 +0900

    📝 [main/Docs] Connectable 개인정보처리방침 추가

--- apps/main/src/components/DotText.tsx
+++ apps/main/src/components/DotText.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 export default function DotText({ children }: { children: ReactNode }) {
   return (
-    <div className="flex ">
+    <div className="flex mt-2">
       <div className="mr-2">· </div> <div>{children}</div>
     </div>
   );
