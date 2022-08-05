740fbcbd33c2d7e5148e0c1ca8bde5c96495fc23
iHoHyeon <hyunee169@naver.com>
2022-08-05 12:00:30 +0900

    ♻️ [main/Refactor] Cookie 존재 여부에 따른 JWT 토큰 전송 핸들링

--- apps/main/src/apis/index.ts
+++ apps/main/src/apis/index.ts
@@ -1,11 +1,17 @@
 import axios from 'axios';
 import { getCookie } from 'cookies-next';
 
-export const authorizationOptions = () => ({
-  headers: {
-    Authorization: `Bearer ${getCookie('auth')}`,
-  },
-});
+export const authorizationOptions = () => {
+  const jwt = getCookie('auth');
+
+  if (!jwt) return;
+
+  return {
+    headers: {
+      Authorization: `Bearer ${jwt}`,
+    },
+  };
+};
 
 const _axiosInstance = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
@@ -16,6 +22,11 @@ const _axiosInstance = axios.create({
 });
 
 _axiosInstance.interceptors.response.use(response => {
+  console.log(response);
+  if (response.status >= 400) {
+    console.log(response.data);
+    throw Error(response.data.message);
+  }
   return response.data;
 });
 
