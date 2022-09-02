ad45bdf82f99fb2ffc5cda6662f39d0f44ffa45c
iHoHyeon <hyunee169@naver.com>
2022-09-02 10:32:46 +0900

    🐛 [main/Fix] 소각된 티켓 매직넘버 제거, 레이아웃 QA

--- apps/main/src/components/Tickets/TicketDetailArticle.tsx
+++ apps/main/src/components/Tickets/TicketDetailArticle.tsx
@@ -1,12 +1,15 @@
 import Image from 'next/image';
 
+import { BUSINESS } from '~/constants/company';
 import { IMAGE_BLUR_DATA_URL } from '~/constants/contents';
+import { KAKAO_TALK_ONE_TO_ONE_CHAT } from '~/constants/link';
 import { EventDetailType } from '~/types/eventType';
 import { Ticket } from '~/types/ticketType';
 import { dayjsKO } from '~/utils/day';
 
 import { PriceText } from '../Events/EventInfo';
 import EventSaleTimer from '../Events/EventSaleTimer';
+import DotText from '../Text/DotText';
 import LinkText from '../Text/LinkText';
 import LinkToKlaytnScope from '../Text/LinkToKlaytnScope';
 import TextInfo from '../Text/TextInfo';
@@ -99,6 +102,7 @@ export default function TicketDetailArticle({ ticketDetail, eventDetail }: Props
       <TempTransaction />
       <TempTransaction />
     </TextInfo.Simple> */}
+      <RefundGuidance />
       <TextInfo
         title="NFT 상세"
         contents={[
@@ -129,3 +133,32 @@ export default function TicketDetailArticle({ ticketDetail, eventDetail }: Props
     </article>
   );
 }
+
+const RefundGuidance = () => {
+  return (
+    <div className="w-full px-2 py-4 text-sm">
+      <h2 className="text-xl font-bold">환불 안내</h2>
+      <ul>
+        <DotText>
+          환불을 위해서는 {BUSINESS.EMAIL} 혹은{' '}
+          <a className="text-blue-500 " href={KAKAO_TALK_ONE_TO_ONE_CHAT} target="_blank" rel="noreferrer">
+            1:1 문의하기
+          </a>{' '}
+          채널로 환불 요청자의 성함, 전화번호를 기재하여 요청해주시기 바랍니다.
+        </DotText>
+        <DotText>
+          취소 수수료 및 구체적인 환불 절차는{' '}
+          <a
+            href="https://quiet-harrier-305.notion.site/Connectable-b0403961e4e24261b763757648dd3231"
+            target="_blank"
+            rel="noreferrer"
+            className="font-bold text-blue-500 underline "
+          >
+            여기
+          </a>
+          를 클릭해주세요.
+        </DotText>
+      </ul>
+    </div>
+  );
+};
